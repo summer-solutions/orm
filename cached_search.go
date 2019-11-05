2909076198ebe224da190e14fa98ee2dc567fd83
latolukasz <latolukasz@gmail.com>
2019-11-05 13:02:14 +0100

    initial commit

--- cached_search.go
+++ cached_search.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"math"
-	"reflect"
 	"strconv"
 	"strings"
 )
@@ -23,154 +22,129 @@ func CachedSearch(entityName string, indexName string, pager Pager, arguments ..
 
 	Where := NewWhere(definition.Query, arguments...)
 	localCache := schema.GetLocalCacheContainer()
+	contextCache := getContextCache()
+	if localCache == nil && contextCache != nil {
+		localCache = contextCache
+	}
 	redisCache := schema.GetRedisCacheContainer()
 	var cacheKey string
 
+	cacheKey = schema.getCacheKeySearch(indexName, Where.GetParameters()...)
+	end := pager.GetPageSize()
+	if start+end > definition.Max {
+		end = totalRows - start
+	}
+	const idsOnCachePage = 1000
+
+	minCachePage := float64((pager.GetCurrentPage() - 1) * pager.GetPageSize() / idsOnCachePage)
+	minCachePageCeil := math.Ceil(minCachePage)
+	maxCachePage := float64((pager.GetCurrentPage()-1)*pager.GetPageSize()+pager.GetPageSize()) / float64(idsOnCachePage)
+	maxCachePageCeil := math.Ceil(maxCachePage)
+	pages := make([]string, 0)
+	filledPages := make(map[string][]uint64)
+	for i := minCachePageCeil; i < maxCachePageCeil; i++ {
+		pages = append(pages, strconv.Itoa(int(i)+1))
+	}
+	var fromCache map[string]interface{}
+	var nilsKeys []string
 	if localCache != nil {
-		cacheKey = schema.getCacheKeySearch(indexName, Where.GetParameters()...)
-		fromCache, has := localCache.Get(cacheKey)
-		if has {
-			ids := fromCache.([]uint64)
-			totalRows = len(ids)
-			if start > totalRows {
-				return make([]interface{}, 0), totalRows
+		lenPages := len(pages)
+		fromCache = make(map[string]interface{}, lenPages)
+		nils := make(map[int]int)
+		nilsKeys = make([]string, 0)
+		i := 0
+		fromCache = localCache.HMget(cacheKey, pages...)
+		index := 0
+		for key, val := range fromCache {
+			if val == nil {
+				nils[index] = i
+				i++
+				nilsKeys = append(nilsKeys, key)
 			}
-			sliceEnd := start + pager.GetPageSize()
-			if sliceEnd > totalRows {
-				sliceEnd = totalRows
+		}
+		if redisCache != nil && len(nilsKeys) > 0 {
+			fromRedis := redisCache.HMget(cacheKey, nilsKeys...)
+			for key, idsFromRedis := range fromRedis {
+				fromCache[key] = idsFromRedis
 			}
-			slice := ids[start:sliceEnd]
-			results = GetByIds(slice, entityName)
-			return
 		}
 	} else if redisCache != nil {
-
-		contextCache := getContextCache()
-
-		cacheKey = schema.getCacheKeySearch(indexName, Where.GetParameters()...)
-		end := pager.GetPageSize()
-		if start+end > definition.Max {
-			end = totalRows - start
-		}
-		const idsOnCachePage = 1000
-
-		minCachePage := float64((pager.GetCurrentPage() - 1) * pager.GetPageSize() / idsOnCachePage)
-		minCachePageCeil := math.Ceil(minCachePage)
-		maxCachePage := float64((pager.GetCurrentPage()-1)*pager.GetPageSize()+pager.GetPageSize()) / float64(idsOnCachePage)
-		maxCachePageCeil := math.Ceil(maxCachePage)
-		pages := make([]string, 0)
-		filledPages := make(map[string][]uint64)
-		for i := minCachePageCeil; i < maxCachePageCeil; i++ {
-			pages = append(pages, strconv.Itoa(int(i)+1))
-		}
-		var fromCache map[string]interface{}
-		var nilsKeys []string
-		if contextCache != nil {
-			lenPages := len(pages)
-			fromCache = make(map[string]interface{}, lenPages)
-			nils := make(map[int]int)
-			nilsKeys = make([]string, 0)
-			i := 0
-			fromCache = contextCache.HMget(cacheKey, pages...)
-			index := 0
-			for key, val := range fromCache {
-				if val == nil {
-					nils[index] = i
-					i++
-					nilsKeys = append(nilsKeys, key)
-				}
-			}
-			if len(nilsKeys) > 0 {
-				fromRedis := redisCache.HMget(cacheKey, nilsKeys...)
-				for key, idsFromRedis := range fromRedis {
-					fromCache[key] = idsFromRedis
-				}
-			}
+		fromCache = redisCache.HMget(cacheKey, pages...)
+	}
+	hasNil := false
+	totalRows = 0
+	for key, idsAsString := range fromCache {
+		if idsAsString == nil {
+			hasNil = true
 		} else {
-			fromCache = redisCache.HMget(cacheKey, pages...)
+			ids := strings.Split(idsAsString.(string), " ")
+			totalRows, _ = strconv.Atoi(ids[0])
+			length := len(ids)
+			idsAsUint := make([]uint64, length-1)
+			for i := 1; i < length; i++ {
+				idsAsUint[i-1], _ = strconv.ParseUint(ids[i], 10, 64)
+			}
+			filledPages[key] = idsAsUint
 		}
-		hasNil := false
-		totalRows = 0
-		for key, idsAsString := range fromCache {
-			if idsAsString == nil {
-				hasNil = true
-			} else {
-				ids := strings.Split(idsAsString.(string), " ")
-				totalRows, _ = strconv.Atoi(ids[0])
-				length := len(ids)
-				idsAsUint := make([]uint64, length-1)
-				for i := 1; i < length; i++ {
-					idsAsUint[i-1], _ = strconv.ParseUint(ids[i], 10, 64)
+	}
+	if hasNil {
+		searchPager := NewPager(1, definition.Max) //TODO select only required rows
+		results, total := SearchIdsWithCount(Where, searchPager, entityName)
+		totalRows = total
+		cacheFields := make(map[string]interface{})
+		for key, ids := range fromCache {
+			if ids == nil {
+				page := key
+				pageInt, _ := strconv.Atoi(page)
+				sliceStart := (pageInt - 1) * idsOnCachePage
+				if sliceStart > total {
+					cacheFields[page] = total
+					continue
 				}
-				filledPages[key] = idsAsUint
+				sliceEnd := sliceStart + idsOnCachePage
+				if sliceEnd > total {
+					sliceEnd = total
+				}
+				values := []uint64{uint64(total)}
+				foundIds := results[sliceStart:sliceEnd]
+				filledPages[key] = foundIds
+				values = append(values, foundIds...)
+				cacheValue := fmt.Sprintf("%v", values)
+				cacheValue = strings.Trim(cacheValue, "[]")
+				cacheFields[page] = cacheValue
 			}
 		}
-		if hasNil {
-			searchPager := NewPager(1, definition.Max)
-			results, total := SearchIdsWithCount(Where, searchPager, entityName)
-			totalRows = total
-			cacheFields := make(map[string]interface{})
-			for key, ids := range fromCache {
-				if ids == nil {
-					page := key
-					pageInt, _ := strconv.Atoi(page)
-					sliceStart := (pageInt - 1) * idsOnCachePage
-					if sliceStart > total {
-						cacheFields[page] = total
-						continue
-					}
-					sliceEnd := sliceStart + idsOnCachePage
-					if sliceEnd > total {
-						sliceEnd = total
-					}
-					values := []uint64{uint64(total)}
-					foundIds := results[sliceStart:sliceEnd]
-					filledPages[key] = foundIds
-					values = append(values, foundIds...)
-					cacheValue := fmt.Sprintf("%v", values)
-					cacheValue = strings.Trim(cacheValue, "[]")
-					cacheFields[page] = cacheValue
-				}
-			}
+		if redisCache != nil {
 			redisCache.HMset(cacheKey, cacheFields)
 		}
-
-		nilKeysLen := len(nilsKeys)
-		if contextCache != nil && nilKeysLen > 0 {
-			fields := make(map[string]interface{}, nilKeysLen)
-			for _, v := range nilsKeys {
-				cacheValue := fmt.Sprintf("%v", filledPages[v])
-				cacheValue = strings.Trim(cacheValue, "[]")
-				fields[v] = cacheValue
-			}
-			contextCache.HMset(cacheKey, fields)
-		}
-
-		resultsIds := make([]uint64, 0, len(filledPages)*idsOnCachePage)
-		for i := minCachePageCeil; i < maxCachePageCeil; i++ {
-			resultsIds = append(resultsIds, filledPages[strconv.Itoa(int(i)+1)]...)
-		}
-		sliceStart := (pager.GetCurrentPage() - 1) * pager.GetPageSize()
-		diff := int(minCachePageCeil) * idsOnCachePage
-		sliceStart -= diff
-		sliceEnd := sliceStart + pager.GetPageSize()
-		length := len(resultsIds)
-		if sliceEnd > length {
-			sliceEnd = length
-		}
-		idsToReturn := resultsIds[sliceStart:sliceEnd]
-		results = GetByIds(idsToReturn, entityName)
-		return
-	} else {
-		panic(fmt.Errorf("cache not defined %s", entityName))
 	}
-	searchPager := NewPager(1, definition.Max)
-	results = Search(Where, searchPager, entityName)
-	totalRows = len(results)
-	ids := make([]uint64, totalRows)
-	for k, v := range results {
-		ids[k] = reflect.ValueOf(v).Field(1).Uint()
+
+	nilKeysLen := len(nilsKeys)
+	if localCache != nil && nilKeysLen > 0 {
+		fields := make(map[string]interface{}, nilKeysLen)
+		for _, v := range nilsKeys {
+			values := []uint64{uint64(totalRows)}
+			values = append(values, filledPages[v]...)
+			cacheValue := fmt.Sprintf("%v", values)
+			cacheValue = strings.Trim(cacheValue, "[]")
+			fields[v] = cacheValue
+		}
+		localCache.HMset(cacheKey, fields)
 	}
-	localCache.Set(cacheKey, ids)
+
+	resultsIds := make([]uint64, 0, len(filledPages)*idsOnCachePage)
+	for i := minCachePageCeil; i < maxCachePageCeil; i++ {
+		resultsIds = append(resultsIds, filledPages[strconv.Itoa(int(i)+1)]...)
+	}
+	sliceStart := (pager.GetCurrentPage() - 1) * pager.GetPageSize()
+	diff := int(minCachePageCeil) * idsOnCachePage
+	sliceStart -= diff
+	sliceEnd := sliceStart + pager.GetPageSize()
+	length := len(resultsIds)
+	if sliceEnd > length {
+		sliceEnd = length
+	}
+	idsToReturn := resultsIds[sliceStart:sliceEnd]
+	results = GetByIds(idsToReturn, entityName)
 	return
 }
