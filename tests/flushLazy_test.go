5623ab5ddf6415d42fad082d89357ba17c67f8ca
latolukasz <latolukasz@gmail.com>
2019-11-10 16:12:57 +0100

    initial commit

--- tests/flushLazy_test.go
+++ tests/flushLazy_test.go
@@ -1,93 +1,89 @@
 package tests
 
-//import (
-//	"github.com/stretchr/testify/assert"
-//	"github.com/summer-solutions/orm"
-//	"strconv"
-//	"testing"
-//)
-//
-//const TestEntityFlushLazyName = "tests.TestEntityFlushLazy"
-//
-//type TestEntityFlushLazy struct {
-//	Orm  orm.ORM `orm:"table=TestFlushLazy;mysql=default;redisCache"`
-//	Id   uint
-//	Name string
-//}
-//
-//func TestFlushLazy(t *testing.T) {
-//	var entity TestEntityFlushLazy
-//	PrepareTables(entity)
-//
-//	LoggerDB := TestDatabaseLogger{}
-//	orm.GetMysqlDB("default").AddLogger(&LoggerDB)
-//	LoggerQueue := TestCacheLogger{}
-//	orm.GetRedisCache("default_queue").AddLogger(&LoggerQueue)
-//
-//	var entities = make([]interface{}, 10)
-//	for i := 1; i <= 10; i++ {
-//		e := TestEntityFlushLazy{Name: "Name " + strconv.Itoa(i)}
-//		entities[i-1] = &e
-//	}
-//	err := orm.FlushLazy(entities...)
-//	assert.Nil(t, err)
-//	assert.Len(t, LoggerDB.Queries, 0)
-//	assert.Len(t, LoggerQueue.Requests, 1)
-//	assert.Equal(t, "LPUSH 1 values lazy_queue", LoggerQueue.Requests[0])
-//
-//	_, found := orm.TryById(1, TestEntityFlushLazyName)
-//	assert.False(t, found)
-//
-//	LazyReceiver := orm.LazyReceiver{RedisName: "default_queue"}
-//	assert.Equal(t, int64(1), LazyReceiver.Size())
-//	err = LazyReceiver.Digest()
-//	assert.Nil(t, err)
-//	assert.Equal(t, int64(0), LazyReceiver.Size())
-//	assert.Len(t, LoggerDB.Queries, 2)
-//	assert.Len(t, LoggerQueue.Requests, 5)
-//	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[2])
-//	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[3])
-//	addedEntity, found := orm.TryById(1, TestEntityFlushLazyName)
-//	assert.True(t, found)
-//	assert.Equal(t, "Name 1", addedEntity.(TestEntityFlushLazy).Name)
-//
-//	LoggerDB.Queries = make([]string, 0)
-//	LoggerQueue.Requests = make([]string, 0)
-//	editedEntity := addedEntity.(TestEntityFlushLazy)
-//	editedEntity.Name = "Name 1.1"
-//	err = orm.FlushLazy(&editedEntity)
-//	assert.Nil(t, err)
-//	assert.Len(t, LoggerDB.Queries, 0)
-//	assert.Len(t, LoggerQueue.Requests, 1)
-//	assert.Equal(t, "LPUSH 1 values lazy_queue", LoggerQueue.Requests[0])
-//
-//	err = LazyReceiver.Digest()
-//	assert.Nil(t, err)
-//	assert.Len(t, LoggerDB.Queries, 1)
-//	assert.Len(t, LoggerQueue.Requests, 3)
-//	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[1])
-//	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[2])
-//	addedEntity, found = orm.TryById(1, TestEntityFlushLazyName)
-//	assert.True(t, found)
-//	assert.Equal(t, "Name 1.1", addedEntity.(TestEntityFlushLazy).Name)
-//
-//	editedEntity = addedEntity.(TestEntityFlushLazy)
-//	LoggerDB.Queries = make([]string, 0)
-//	LoggerQueue.Requests = make([]string, 0)
-//	orm.MarkToDelete(editedEntity)
-//	err = orm.FlushLazy(&editedEntity)
-//	assert.Nil(t, err)
-//	assert.Len(t, LoggerDB.Queries, 0)
-//	assert.Len(t, LoggerQueue.Requests, 1)
-//	assert.Equal(t, "LPUSH 1 values lazy_queue", LoggerQueue.Requests[0])
-//
-//	err = LazyReceiver.Digest()
-//	assert.Nil(t, err)
-//	assert.Len(t, LoggerDB.Queries, 1)
-//	assert.Len(t, LoggerQueue.Requests, 3)
-//	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[1])
-//	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[2])
-//	addedEntity, found = orm.TryById(1, TestEntityFlushLazyName)
-//	assert.False(t, found)
-//
-//}
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/summer-solutions/orm"
+	"strconv"
+	"testing"
+)
+
+type TestEntityFlushLazy struct {
+	Orm  orm.ORM `orm:"table=TestFlushLazy;mysql=default;redisCache"`
+	Id   uint
+	Name string
+}
+
+func TestFlushLazy(t *testing.T) {
+	var entity TestEntityFlushLazy
+	PrepareTables(entity)
+
+	LoggerDB := TestDatabaseLogger{}
+	orm.GetMysqlDB("default").AddLogger(&LoggerDB)
+	LoggerQueue := TestCacheLogger{}
+	orm.GetRedisCache("default_queue").AddLogger(&LoggerQueue)
+
+	var entities = make([]interface{}, 10)
+	for i := 1; i <= 10; i++ {
+		e := TestEntityFlushLazy{Name: "Name " + strconv.Itoa(i)}
+		entities[i-1] = &e
+	}
+	err := orm.FlushLazy(entities...)
+	assert.Nil(t, err)
+	assert.Len(t, LoggerDB.Queries, 0)
+	assert.Len(t, LoggerQueue.Requests, 1)
+	assert.Equal(t, "LPUSH 1 values lazy_queue", LoggerQueue.Requests[0])
+
+	found := orm.TryById(1, &entity)
+	assert.False(t, found)
+
+	LazyReceiver := orm.LazyReceiver{RedisName: "default_queue"}
+	assert.Equal(t, int64(1), LazyReceiver.Size())
+	err = LazyReceiver.Digest()
+	assert.Nil(t, err)
+	assert.Equal(t, int64(0), LazyReceiver.Size())
+	assert.Len(t, LoggerDB.Queries, 2)
+	assert.Len(t, LoggerQueue.Requests, 5)
+	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[2])
+	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[3])
+	found = orm.TryById(1, &entity)
+	assert.True(t, found)
+	assert.Equal(t, "Name 1", entity.Name)
+
+	LoggerDB.Queries = make([]string, 0)
+	LoggerQueue.Requests = make([]string, 0)
+	entity.Name = "Name 1.1"
+	err = orm.FlushLazy(&entity)
+	assert.Nil(t, err)
+	assert.Len(t, LoggerDB.Queries, 0)
+	assert.Len(t, LoggerQueue.Requests, 1)
+	assert.Equal(t, "LPUSH 1 values lazy_queue", LoggerQueue.Requests[0])
+
+	err = LazyReceiver.Digest()
+	assert.Nil(t, err)
+	assert.Len(t, LoggerDB.Queries, 1)
+	assert.Len(t, LoggerQueue.Requests, 3)
+	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[1])
+	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[2])
+	found = orm.TryById(1, &entity)
+	assert.True(t, found)
+	assert.Equal(t, "Name 1.1", entity.Name)
+
+	LoggerDB.Queries = make([]string, 0)
+	LoggerQueue.Requests = make([]string, 0)
+	orm.MarkToDelete(&entity)
+	err = orm.FlushLazy(&entity)
+	assert.Nil(t, err)
+	assert.Len(t, LoggerDB.Queries, 0)
+	assert.Len(t, LoggerQueue.Requests, 1)
+	assert.Equal(t, "LPUSH 1 values lazy_queue", LoggerQueue.Requests[0])
+
+	err = LazyReceiver.Digest()
+	assert.Nil(t, err)
+	assert.Len(t, LoggerDB.Queries, 1)
+	assert.Len(t, LoggerQueue.Requests, 3)
+	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[1])
+	assert.Equal(t, "RPOP lazy_queue", LoggerQueue.Requests[2])
+	found = orm.TryById(1, &entity)
+	assert.False(t, found)
+
+}
