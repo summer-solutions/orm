6baab11f8aa0cee26882127013fdfd8831c3cc81
latolukasz <latolukasz@gmail.com>
2019-11-17 19:21:07 +0100

    initial commit

--- localCache.go
+++ localCache.go
@@ -3,6 +3,7 @@ package orm
 import (
 	"fmt"
 	"github.com/golang/groupcache/lru"
+	"time"
 )
 
 type LocalCache struct {
@@ -14,16 +15,18 @@ type LocalCache struct {
 }
 
 func (c *LocalCache) Get(key string) (value interface{}, ok bool) {
+	start := time.Now()
 	value, ok = c.lru.Get(key)
 	misses := 0
 	if !ok {
 		misses = 1
 	}
-	c.log(key, "GET", 0, misses)
+	c.log(key, "GET", time.Now().Sub(start).Microseconds(), misses)
 	return
 }
 
 func (c *LocalCache) MGet(keys ...string) map[string]interface{} {
+	start := time.Now()
 	results := make(map[string]interface{}, len(keys))
 	misses := 0
 	for _, key := range keys {
@@ -34,16 +37,18 @@ func (c *LocalCache) MGet(keys ...string) map[string]interface{} {
 		}
 		results[key] = value
 	}
-	c.log(fmt.Sprintf("%v", keys), "MGET", 0, misses)
+	c.log(fmt.Sprintf("%v", keys), "MGET", time.Now().Sub(start).Microseconds(), misses)
 	return results
 }
 
 func (c *LocalCache) Set(key string, value interface{}) {
+	start := time.Now()
 	c.lru.Add(key, value)
-	c.log(key, "ADD", 0, 0)
+	c.log(key, "ADD", time.Now().Sub(start).Microseconds(), 0)
 }
 
 func (c *LocalCache) MSet(pairs ...interface{}) {
+	start := time.Now()
 	max := len(pairs)
 	for i := 0; i < max; i += 2 {
 		c.lru.Add(pairs[i], pairs[i+1])
@@ -56,11 +61,12 @@ func (c *LocalCache) MSet(pairs ...interface{}) {
 			keys[j] = pairs[i].(string)
 			j++
 		}
-		c.log(fmt.Sprintf("%v", keys), "MSET", 0, 0)
+		c.log(fmt.Sprintf("%v", keys), "MSET", time.Now().Sub(start).Microseconds(), 0)
 	}
 }
 
 func (c *LocalCache) HMget(key string, fields ...string) map[string]interface{} {
+	start := time.Now()
 	l := len(fields)
 	results := make(map[string]interface{}, l)
 	value, ok := c.lru.Get(key)
@@ -80,12 +86,13 @@ func (c *LocalCache) HMget(key string, fields ...string) map[string]interface{} 
 		}
 	}
 	if c.loggers != nil {
-		c.log(key, fmt.Sprintf("HMGET %v", fields), 0, misses)
+		c.log(key, fmt.Sprintf("HMGET %v", fields), time.Now().Sub(start).Microseconds(), misses)
 	}
 	return results
 }
 
 func (c *LocalCache) HMset(key string, fields map[string]interface{}) {
+	start := time.Now()
 	m, has := c.lru.Get(key)
 	if !has {
 		m = make(map[string]interface{})
@@ -101,25 +108,28 @@ func (c *LocalCache) HMset(key string, fields map[string]interface{}) {
 			keys[i] = key
 			i++
 		}
-		c.log(key, fmt.Sprintf("HMSET %v", keys), 0, 0)
+		c.log(key, fmt.Sprintf("HMSET %v", keys), time.Now().Sub(start).Microseconds(), 0)
 	}
 }
 
 func (c *LocalCache) Remove(key string) {
+	start := time.Now()
 	c.lru.Remove(key)
-	c.log(key, "REMOVE", 0, 0)
+	c.log(key, "REMOVE", time.Now().Sub(start).Microseconds(), 0)
 }
 
 func (c *LocalCache) RemoveMany(keys ...string) {
+	start := time.Now()
 	for _, v := range keys {
 		c.lru.Remove(v)
 	}
-	c.log("", fmt.Sprintf("REMOVE MANY %v", keys), 0, 0)
+	c.log("", fmt.Sprintf("REMOVE MANY %v", keys), time.Now().Sub(start).Microseconds(), 0)
 }
 
 func (c *LocalCache) Clear() {
+	start := time.Now()
 	c.lru.Clear()
-	c.log("", "CLEAR", 0, 0)
+	c.log("", "CLEAR", time.Now().Sub(start).Microseconds(), 0)
 }
 
 func (c *LocalCache) AddLogger(logger CacheLogger) {
