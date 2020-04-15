f8707936b35af87b6e834c697e909f5750e08376
summer-solutions <lukasz.lato@summer-solutions.com>
2020-04-15 15:50:12 +0200

    better coverage

--- tests/yaml_loader_test.go
+++ tests/yaml_loader_test.go
@@ -30,4 +30,58 @@ func TestYamlLoader(t *testing.T) {
 	schema, has := config.GetTableSchema("test")
 	assert.Nil(t, schema)
 	assert.False(t, has)
+
+	invalidMap := make(map[string]interface{})
+	invalidMap["ss"] = "vv"
+	registry, err = orm.InitByYaml(invalidMap)
+	assert.Nil(t, registry)
+	assert.EqualError(t, err, "invalid orm section in config")
+
+	invalidMySQL := make(map[string]interface{})
+	invalidMySQL["default"] = map[interface{}]interface{}{"mysql": 12}
+	registry, err = orm.InitByYaml(invalidMySQL)
+	assert.Nil(t, registry)
+	assert.EqualError(t, err, "invalid mysql uri: 12")
+
+	invalidRedis := make(map[string]interface{})
+	invalidRedis["default"] = map[interface{}]interface{}{"redis": 12}
+	registry, err = orm.InitByYaml(invalidRedis)
+	assert.Nil(t, registry)
+	assert.EqualError(t, err, "invalid redis uri: 12")
+
+	invalidRedis = make(map[string]interface{})
+	invalidRedis["default"] = map[interface{}]interface{}{"redis": "test"}
+	registry, err = orm.InitByYaml(invalidRedis)
+	assert.Nil(t, registry)
+	assert.EqualError(t, err, "invalid redis uri: test")
+
+	invalidRedis = make(map[string]interface{})
+	invalidRedis["default"] = map[interface{}]interface{}{"redis": "test:ss:dd"}
+	registry, err = orm.InitByYaml(invalidRedis)
+	assert.Nil(t, registry)
+	assert.EqualError(t, err, "invalid redis DB id: test:ss:dd")
+
+	invalidLazyQueue := make(map[string]interface{})
+	invalidLazyQueue["default"] = map[interface{}]interface{}{"lazyQueue": 12}
+	registry, err = orm.InitByYaml(invalidLazyQueue)
+	assert.Nil(t, registry)
+	assert.EqualError(t, err, "invalid orm value for default: 12")
+
+	invalidLocker := make(map[string]interface{})
+	invalidLocker["default"] = map[interface{}]interface{}{"locker": 12}
+	registry, err = orm.InitByYaml(invalidLocker)
+	assert.Nil(t, registry)
+	assert.EqualError(t, err, "invalid orm value for default: 12")
+
+	invalidDirtyQueue := make(map[string]interface{})
+	invalidDirtyQueue["default"] = map[interface{}]interface{}{"dirtyQueue": 12}
+	registry, err = orm.InitByYaml(invalidDirtyQueue)
+	assert.Nil(t, registry)
+	assert.EqualError(t, err, "invalid orm value for default: 12")
+
+	invalidLocalCache := make(map[string]interface{})
+	invalidLocalCache["default"] = map[interface{}]interface{}{"localCache": "test"}
+	registry, err = orm.InitByYaml(invalidLocalCache)
+	assert.Nil(t, registry)
+	assert.EqualError(t, err, "invalid orm value for default: test")
 }
--- yamlLoader.go
+++ yamlLoader.go
@@ -7,60 +7,48 @@ import (
 )
 
 func InitByYaml(yaml map[string]interface{}) (registry *Registry, err error) {
-	configData, has := yaml["orm"]
-	if !has {
-		return nil, fmt.Errorf("missing orm section in config")
-	}
-	asMap, ok := configData.(map[interface{}]interface{})
-	if !ok {
-		return nil, fmt.Errorf("invalid orm section in config")
-	}
 	registry = &Registry{}
-	for key, data := range asMap {
+	for key, data := range yaml {
 		dataAsMap, ok := data.(map[interface{}]interface{})
-		if !ok {
-			return nil, fmt.Errorf("invalid orm section in config")
-		}
-		keyAsString, ok := key.(string)
 		if !ok {
 			return nil, fmt.Errorf("invalid orm section in config")
 		}
 		for dataKey, value := range dataAsMap {
 			switch dataKey {
 			case "mysql":
-				err := validateOrmMysqlURI(registry, value, keyAsString)
+				err := validateOrmMysqlURI(registry, value, key)
 				if err != nil {
 					return nil, err
 				}
 			case "redis":
-				err := validateRedisURI(registry, value, keyAsString)
+				err := validateRedisURI(registry, value, key)
 				if err != nil {
 					return nil, err
 				}
 			case "lazyQueue":
-				valAsString, err := validateOrmString(value, keyAsString)
+				valAsString, err := validateOrmString(value, key)
 				if err != nil {
 					return nil, err
 				}
-				registry.RegisterLazyQueue(keyAsString, valAsString)
+				registry.RegisterLazyQueue(key, valAsString)
 			case "locker":
-				valAsString, err := validateOrmString(value, keyAsString)
+				valAsString, err := validateOrmString(value, key)
 				if err != nil {
 					return nil, err
 				}
-				registry.RegisterLocker(keyAsString, valAsString)
+				registry.RegisterLocker(key, valAsString)
 			case "dirtyQueue":
-				valAsString, err := validateOrmString(value, keyAsString)
+				valAsString, err := validateOrmString(value, key)
 				if err != nil {
 					return nil, err
 				}
-				registry.RegisterDirtyQueue(keyAsString, &RedisDirtyQueueSender{PoolName: valAsString})
+				registry.RegisterDirtyQueue(key, &RedisDirtyQueueSender{PoolName: valAsString})
 			case "localCache":
-				number, err := validateOrmInt(value, keyAsString)
+				number, err := validateOrmInt(value, key)
 				if err != nil {
 					return nil, err
 				}
-				registry.RegisterLocalCache(number, keyAsString)
+				registry.RegisterLocalCache(number, key)
 			}
 		}
 	}
@@ -79,12 +67,15 @@ func validateOrmMysqlURI(registry *Registry, value interface{}, key string) erro
 func validateRedisURI(registry *Registry, value interface{}, key string) error {
 	asString, ok := value.(string)
 	if !ok {
-		return fmt.Errorf("invalid mysql uri: %v", value)
+		return fmt.Errorf("invalid redis uri: %v", value)
 	}
 	elements := strings.Split(asString, ":")
+	if len(elements) != 3 {
+		return fmt.Errorf("invalid redis uri: %v", value)
+	}
 	db, err := strconv.ParseUint(elements[2], 10, 64)
 	if err != nil {
-		return fmt.Errorf("invalid redis uri: %v", value)
+		return fmt.Errorf("invalid redis DB id: %v", value)
 	}
 	uri := fmt.Sprintf("%s:%s", elements[0], elements[1])
 	registry.RegisterRedis(uri, int(db), key)
