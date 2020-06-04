141802eb9894d80b54ae001d334553cbc424da8d
summer-solutions <lukasz.lato@summer-solutions.com>
2020-06-04 10:11:07 +0200

    fixed clear cache

--- logger.go
+++ logger.go
@@ -1,7 +1,15 @@
 package orm
 
 import (
+	"fmt"
+	"net/http"
 	"os"
+	"reflect"
+	"runtime/debug"
+	"strings"
+	"time"
+
+	"github.com/juju/errors"
 
 	apexLog "github.com/apex/log"
 	"github.com/apex/log/handlers/multi"
@@ -18,14 +26,14 @@ type Log interface {
 	Debug(message string, fields apexLog.Fielder)
 	Info(message string, fields apexLog.Fielder)
 	Warn(message string, fields apexLog.Fielder)
-	Error(message string, err error, fields apexLog.Fielder)
-	Fatal(message string, err error, fields apexLog.Fielder)
+	Error(err interface{}, fields apexLog.Fielder)
+	ErrorMessage(message string, fields apexLog.Fielder)
 }
 
 func newLog(engine *Engine) *log {
 	multiHandler := multi.New()
 	l := &apexLog.Logger{Handler: multiHandler, Level: apexLog.DebugLevel}
-	entry := apexLog.NewEntry(l)
+	entry := apexLog.NewEntry(l).WithFields(apexLog.Fields{"logger.name": "github.com/summer-solutions/orm", "logger.thread_name": time.Now().UnixNano()})
 	return &log{engine, &logger{log: entry, handler: multiHandler}}
 }
 
@@ -33,44 +41,73 @@ func (l *log) AddFields(fields apexLog.Fielder) {
 	l.logger.log = l.logger.log.WithFields(fields)
 }
 
+func (l *log) AddFieldsFromHTTPRequest(r *http.Request, ip string) {
+	fields := apexLog.Fields{"http.url": r.RequestURI}
+	fields["http.path"] = r.URL.Path
+	fields["http.useragent"] = r.UserAgent()
+	fields["http.method"] = r.Method
+	fields["http.host"] = r.Host
+	fields["http.query"] = r.URL.Query()
+	fields["network.client.ip"] = ip
+	l.AddFields(fields)
+}
+
 func (l *log) Debug(message string, fields apexLog.Fielder) {
+	log := l.logger.log
 	if fields != nil {
-		l.logger.log.WithFields(fields).Debug(message)
-		return
+		log = log.WithFields(fields)
 	}
-	l.logger.log.Debug(message)
+	log.Debug(message)
 }
 
 func (l *log) Info(message string, fields apexLog.Fielder) {
+	log := l.logger.log
 	if fields != nil {
-		l.logger.log.WithFields(fields).Info(message)
-		return
+		log = log.WithFields(fields)
 	}
-	l.logger.log.Info(message)
+	log.Info(message)
 }
 
 func (l *log) Warn(message string, fields apexLog.Fielder) {
+	log := l.logger.log
 	if fields != nil {
-		l.logger.log.WithFields(fields).Warn(message)
-		return
+		log = log.WithFields(fields)
 	}
-	l.logger.log.Warn(message)
+	log.Warn(message)
 }
 
-func (l *log) Error(message string, err error, fields apexLog.Fielder) {
+func (l *log) ErrorMessage(message string, fields apexLog.Fielder) {
+	log := l.logger.log
 	if fields != nil {
-		l.logger.log.WithFields(fields).WithError(err).Error(message)
-		return
+		log = log.WithFields(fields)
 	}
-	l.logger.log.WithError(err).Error(message)
+	log.Error(message)
 }
 
-func (l *log) Fatal(message string, err error, fields apexLog.Fielder) {
+func (l *log) Error(err interface{}, fields apexLog.Fielder) {
+	log := l.logger.log
 	if fields != nil {
-		l.logger.log.WithFields(fields).WithError(err).Fatal(message)
+		log = log.WithFields(fields)
+	}
+	lines := strings.Split(string(debug.Stack()), "\n")[2:]
+	fullStack := strings.Join(lines, "\n")
+	asErr, ok := err.(error)
+	if ok {
+		stackParts := strings.Split(errors.ErrorStack(asErr), "\n")
+		details := strings.Join(stackParts[1:], "\n")
+		errorFields := apexLog.Fields{"error.message": asErr.Error()}
+		errorFields["error.stack"] = fullStack
+		errorFields["error.details"] = details
+		errorFields["error.kind"] = reflect.TypeOf(errors.Cause(asErr)).String()
+		log = log.WithFields(errorFields)
+		log.Error(asErr.Error())
 		return
 	}
-	l.logger.log.WithError(err).Fatal(message)
+	message := fmt.Sprintf("%v", err)
+	errorFields := apexLog.Fields{"error.message": message}
+	errorFields["error.stack"] = fullStack
+	errorFields["error.kind"] = "panicRecovery"
+	log.Error(message)
 }
 
 type jsonHandler struct{}
