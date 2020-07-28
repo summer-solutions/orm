9b74d24e6c58566a53e66a3c27b99ccd4449a0dd
summer-solutions <lukasz.lato@summer-solutions.com>
2020-07-28 12:48:57 +0200

    more tests

---
+++ where_test.go
@@ -0,0 +1,16 @@
+package orm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWhere(t *testing.T) {
+	where := NewWhere("1 AND Field = ? AND Field2 IN ?", 2, []string{"a", "b"})
+	assert.Equal(t, "1 AND Field = ? AND Field2 IN (?,?)", where.String())
+	assert.Equal(t, []interface{}{2, "a", "b"}, where.GetParameters())
+	where.Append("AND Field3 = ? AND Field4 IN ?", "c", []string{"d", "e"})
+	assert.Equal(t, "1 AND Field = ? AND Field2 IN (?,?) AND Field3 = ? AND Field4 IN (?,?)", where.String())
+	assert.Equal(t, []interface{}{2, "a", "b", "c", "d", "e"}, where.GetParameters())
+}
