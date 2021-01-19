e1da665c8a8a47e78a9fff3b961a58212b153202
Lukasz Lato <lukasz.lato@summer-solutions.com>
2021-01-20 00:01:22 +0100

    newest go plugins

--- tools/redis_statistics.go
+++ tools/redis_statistics.go
@@ -1,21 +1,35 @@
 package tools
 
 import (
+	"strings"
+
 	"github.com/summer-solutions/orm"
 )
 
 type RedisStatistics struct {
 	RedisPool string
-	Info      string
+	Info      map[string]string
 }
 
 func GetRedisStatistics(engine *orm.Engine) []*RedisStatistics {
 	pools := engine.GetRegistry().GetRedisPools()
 	results := make([]*RedisStatistics, len(pools))
 	for i, pool := range pools {
-		poolStats := &RedisStatistics{RedisPool: pool}
+		poolStats := &RedisStatistics{RedisPool: pool, Info: make(map[string]string)}
 		r := engine.GetRedis(pool)
-		poolStats.Info = r.Info("everything")
+		info := r.Info("everything")
+		lines := strings.Split(info, "\r\n")
+		for _, line := range lines {
+			if line == "" || strings.HasPrefix(line, "#") {
+				continue
+			}
+			row := strings.Split(line, ":")
+			val := ""
+			if len(row) > 1 {
+				val = row[1]
+			}
+			poolStats.Info[row[0]] = val
+		}
 		results[i] = poolStats
 	}
 	return results
