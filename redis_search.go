3608449a58a32e39f9a584c4a88271057b369025
Lukasz Lato <lukasz.lato@summer-solutions.com>
2021-02-15 14:55:03 +0100

    redis search

--- redis_search.go
+++ redis_search.go
@@ -2,6 +2,11 @@ package orm
 
 import (
 	"context"
+	"strconv"
+	"strings"
+	"time"
+
+	apexLog "github.com/apex/log"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -18,23 +23,275 @@ type RedisSearchIndex struct {
 	RedisPool string
 }
 
-func (r *RedisSearch) CreateIndex(index RedisSearchIndex) string {
-	cmd := redis.NewStringCmd(r.ctx, "FT.CREATE", "myIdx", "ON", "HASH",
+type RedisSearchIndexAlter struct {
+	search  *RedisSearch
+	Query   string
+	Safe    bool
+	Pool    string
+	Execute func()
+}
+
+type RedisSearchIndexInfo struct {
+	Name                     string
+	Options                  []interface{}
+	Definition               RedisSearchIndexInfoDefinition
+	Fields                   []RedisSearchIndexInfoField
+	NumDocs                  uint64
+	MaxDocID                 uint64
+	NumTerms                 uint64
+	NumRecords               uint64
+	InvertedSzMB             float64
+	TotalInvertedIndexBlocks float64
+	OffsetVectorsSzMB        float64
+	DocTableSizeMB           float64
+	SortableValuesSizeMB     float64
+	KeyTableSizeMB           float64
+	RecordsPerDocAvg         float64
+	BytesPerRecordAvg        float64
+	OffsetsPerTermAvg        float64
+	OffsetBitsPerRecordAvg   float64
+	HashIndexingFailures     uint64
+	Indexing                 uint64
+	PercentIndexed           float64
+}
+
+type RedisSearchIndexInfoDefinition struct {
+	KeyType       string
+	Prefixes      []string
+	LanguageField string
+	ScoreField    string
+	PayloadField  string
+	DefaultScore  int
+}
+
+type RedisSearchIndexInfoField struct {
+	Name   string
+	Type   string
+	Weight int
+}
+
+func (r *RedisSearch) createIndex(index RedisSearchIndex) string {
+	cmd := redis.NewStringCmd(r.ctx, "FT.CREATE", index.Name, "ON", "HASH",
 		"PREFIX", "1", "doc:", "SCHEMA", "title", "TEXT", "WEIGHT", "5.0", "body",
 		"TEXT", "url", "TEXT")
+	start := time.Now()
 	err := r.client.Process(r.ctx, cmd)
+	if r.engine.hasRedisLogger {
+		r.fillLogFields("[ORM][REDIS-SEARCH][FT.CREATE]", start, "ft_create", 1,
+			map[string]interface{}{"Index": index.Name}, err)
+	}
 	checkError(err)
 	res, err := cmd.Result()
 	checkError(err)
 	return res
 }
 
-type RedisSearchIndexAlter struct {
-	Query string
-	Safe  bool
-	Pool  string
+func (r *RedisSearch) listIndices() []string {
+	cmd := redis.NewStringSliceCmd(r.ctx, "FT._LIST")
+	start := time.Now()
+	err := r.client.Process(r.ctx, cmd)
+	if r.engine.hasRedisLogger {
+		r.fillLogFields("[ORM][REDIS-SEARCH][FT.LIST]", start, "ft_list", 1, nil, err)
+	}
+	checkError(err)
+	res, err := cmd.Result()
+	checkError(err)
+	return res
+}
+
+func (r *RedisSearch) dropIndex(indexName string) string {
+	cmd := redis.NewStringCmd(r.ctx, "FT.DROPINDEX", indexName, "DD")
+	start := time.Now()
+	err := r.client.Process(r.ctx, cmd)
+	if r.engine.hasRedisLogger {
+		r.fillLogFields("[ORM][REDIS-SEARCH][FT.DROPINDEX]", start, "ft_dropindex", 1,
+			apexLog.Fields{"Index": indexName}, err)
+	}
+	checkError(err)
+	res, err := cmd.Result()
+	checkError(err)
+	return res
+}
+
+func (r *RedisSearch) info(indexName string) RedisSearchIndexInfo {
+	cmd := redis.NewSliceCmd(r.ctx, "FT.INFO", indexName)
+	start := time.Now()
+	err := r.client.Process(r.ctx, cmd)
+	if r.engine.hasRedisLogger {
+		r.fillLogFields("[ORM][REDIS-SEARCH][FT.INFO]", start, "ft_info", 1,
+			apexLog.Fields{"Index": indexName}, err)
+	}
+	checkError(err)
+	res, err := cmd.Result()
+	checkError(err)
+	info := RedisSearchIndexInfo{}
+	for i, row := range res {
+		switch row {
+		case "index_name":
+			info.Name = res[i+1].(string)
+		case "index_options":
+			info.Options = res[i+1].([]interface{})
+		case "index_definition":
+			def := res[i+1].([]interface{})
+			definition := RedisSearchIndexInfoDefinition{}
+			for subKey, subValue := range def {
+				switch subValue {
+				case "key_type":
+					definition.KeyType = def[subKey+1].(string)
+				case "prefixes":
+					prefixesRaw := def[subKey+1].([]interface{})
+					prefixes := make([]string, len(prefixesRaw))
+					for k, v := range prefixesRaw {
+						prefixes[k] = v.(string)
+					}
+					definition.Prefixes = prefixes
+				case "language_field":
+					definition.LanguageField = def[subKey+1].(string)
+				case "default_score":
+					score, _ := strconv.Atoi(def[subKey+1].(string))
+					definition.DefaultScore = score
+				case "score_field":
+					definition.ScoreField = def[subKey+1].(string)
+				case "payload_field":
+					definition.PayloadField = def[subKey+1].(string)
+				}
+			}
+			info.Definition = definition
+		case "fields":
+			fieldsRaw := res[i+1].([]interface{})
+			fields := make([]RedisSearchIndexInfoField, len(fieldsRaw))
+			for i, v := range fieldsRaw {
+				def := v.([]interface{})
+				field := RedisSearchIndexInfoField{Name: def[0].(string)}
+				def = def[1:]
+				for subKey, subValue := range def {
+					switch subValue {
+					case "type":
+						field.Type = def[subKey+1].(string)
+					case "WEIGHT":
+						weight, _ := strconv.Atoi(def[subKey+1].(string))
+						field.Weight = weight
+					}
+				}
+				fields[i] = field
+			}
+			info.Fields = fields
+		case "num_docs":
+			v, _ := strconv.ParseUint(res[i+1].(string), 10, 64)
+			info.NumDocs = v
+		case "max_doc_id":
+			v, _ := strconv.ParseUint(res[i+1].(string), 10, 64)
+			info.MaxDocID = v
+		case "num_terms":
+			v, _ := strconv.ParseUint(res[i+1].(string), 10, 64)
+			info.NumTerms = v
+		case "num_records":
+			v, _ := strconv.ParseUint(res[i+1].(string), 10, 64)
+			info.NumRecords = v
+		case "inverted_sz_mb":
+			v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+			info.InvertedSzMB = v
+		case "total_inverted_index_blocks":
+			v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+			info.TotalInvertedIndexBlocks = v
+		case "offset_vectors_sz_mb":
+			v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+			info.OffsetVectorsSzMB = v
+		case "doc_table_size_mb":
+			v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+			info.DocTableSizeMB = v
+		case "sortable_values_size_mb":
+			v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+			info.SortableValuesSizeMB = v
+		case "key_table_size_mb":
+			v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+			info.KeyTableSizeMB = v
+		case "records_per_doc_avg":
+			if res[i+1] != "-nan" {
+				v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+				info.RecordsPerDocAvg = v
+			}
+		case "bytes_per_record_avg":
+			if res[i+1] != "-nan" {
+				v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+				info.BytesPerRecordAvg = v
+			}
+		case "offsets_per_term_avg":
+			if res[i+1] != "-nan" {
+				v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+				info.OffsetsPerTermAvg = v
+			}
+		case "offset_bits_per_record_avg":
+			if res[i+1] != "-nan" {
+				v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+				info.OffsetBitsPerRecordAvg = v
+			}
+		case "hash_indexing_failures":
+			v, _ := strconv.ParseUint(res[i+1].(string), 10, 64)
+			info.HashIndexingFailures = v
+		case "indexing":
+			v, _ := strconv.ParseUint(res[i+1].(string), 10, 64)
+			info.Indexing = v
+		case "percent_indexed":
+			v, _ := strconv.ParseFloat(res[i+1].(string), 64)
+			info.PercentIndexed = v
+		}
+	}
+	return info
 }
 
 func getRedisSearchAlters(engine *Engine) (alters []RedisSearchIndexAlter) {
-	return nil
+	alters = make([]RedisSearchIndexAlter, 0)
+	for _, poolName := range engine.GetRegistry().GetRedisPools() {
+		r := engine.GetRedis(poolName)
+		info := r.Info("Modules")
+		lines := strings.Split(info, "\r\n")
+		hasModule := false
+		for _, line := range lines {
+			if strings.HasPrefix(line, "module:name=search") {
+				hasModule = true
+				break
+			}
+		}
+		if !hasModule {
+			continue
+		}
+		search := engine.GetRedisSearch(poolName)
+		for _, name := range search.listIndices() {
+			_, has := engine.registry.redisSearchIndexes[name]
+			if !has {
+				query := "FT.DROPINDEX " + name + " DD"
+				alter := RedisSearchIndexAlter{Pool: poolName, Query: query, search: search}
+				info := search.info(name)
+				alter.Safe = info.NumDocs == 0 && info.Indexing == 0
+				alter.Execute = func() {
+					alter.search.dropIndex(name)
+				}
+				alters = append(alters, alter)
+			}
+		}
+	}
+	return alters
+}
+
+func (r *RedisSearch) fillLogFields(message string, start time.Time, operation string, keys int, fields apexLog.Fields, err error) {
+	now := time.Now()
+	stop := time.Since(start).Microseconds()
+	e := r.engine.queryLoggers[QueryLoggerSourceRedis].log.WithFields(apexLog.Fields{
+		"microseconds": stop,
+		"operation":    operation,
+		"pool":         r.code,
+		"keys":         keys,
+		"target":       "redis",
+		"started":      start.UnixNano(),
+		"finished":     now.UnixNano(),
+	})
+	if fields != nil {
+		e = e.WithFields(fields)
+	}
+	if err != nil {
+		injectLogError(err, e).Error(message)
+	} else {
+		e.Info(message)
+	}
 }
--- redis_search_test.go
+++ redis_search_test.go
@@ -3,6 +3,9 @@ package orm
 import (
 	"testing"
 
+	apexLog "github.com/apex/log"
+	"github.com/apex/log/handlers/memory"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,8 +16,23 @@ func TestRedisSearch(t *testing.T) {
 	assert.NoError(t, err)
 	engine := validatedRegistry.CreateEngine()
 	engine.GetRedis().FlushDB()
+
+	testLog := memory.New()
+	engine.AddQueryLogger(testLog, apexLog.InfoLevel, QueryLoggerSourceRedis)
+
 	search := engine.GetRedisSearch()
 	assert.NotNil(t, search)
 	alters := engine.GetRedisSearchIndexAlters()
-	assert.Nil(t, alters)
+	assert.Len(t, alters, 0)
+
+	testIndex := RedisSearchIndex{Name: "test", RedisPool: "default"}
+	search.createIndex(testIndex)
+	alters = engine.GetRedisSearchIndexAlters()
+	assert.Len(t, alters, 1)
+	assert.Equal(t, "default", alters[0].Pool)
+	assert.Equal(t, "FT.DROPINDEX test DD", alters[0].Query)
+	assert.True(t, alters[0].Safe)
+	alters[0].Execute()
+	alters = engine.GetRedisSearchIndexAlters()
+	assert.Len(t, alters, 0)
 }
