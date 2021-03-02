2db99069e35b51e78e981618a9185f7a6fa7c0ed
Lukasz Lato <lukasz.lato@summer-solutions.com>
2021-03-02 08:59:11 +0100

    multi update

--- event_broker_test.go
+++ event_broker_test.go
@@ -234,6 +234,7 @@ func TestRedisStreamGroupConsumer(t *testing.T) {
 		events[2].Skip()
 		events[3].Skip()
 		events[4].Skip()
+		time.Sleep(time.Millisecond * 10)
 	})
 	assert.Equal(t, 2, iterations)
 	assert.Equal(t, 1, heartBeats)
